e6b82dd4161fffd2a9d0e70d72fbec6c915008e5
anneclaire <s36093@stagiaire.aelion.fr>
2024-03-29 11:55:23 +0100

    POE_US2-array with some poe added and implement method get to display the list in the template

--- src/app/poe/services/poe.service.ts
+++ src/app/poe/services/poe.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { IService } from '../../core/interfaces/i-service';
+import { Poe } from '../poe_types/poe_types';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PoeService {
+export class PoeService implements IService<Poe> {
+  private _poe: Array<Poe> = [
+    {
+      label: 'Développeur en cybersécurité',
+      duration: 457,
+    },
+    {
+      label: 'Développeur web',
+      duration: 457,
+    },
+    {
+      label: 'Management',
+      duration: 80,
+    },
+    {
+      label: 'Git',
+      duration: 35,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
+  add(item: Poe): void {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Observable<Poe[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  get poe(): Array<Poe> {
+    return this._poe;
+  }
 }
