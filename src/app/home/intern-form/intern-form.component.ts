7437f07bf1e70caa08386b2ede8f246cb5988216
anneclaire <s36093@stagiaire.aelion.fr>
2024-03-29 13:43:24 +0100

    Remove comments

--- src/app/home/intern-form/intern-form.component.ts
+++ src/app/home/intern-form/intern-form.component.ts
@@ -12,22 +12,15 @@ import { Intern } from '../types/intern.type';
   styleUrl: './intern-form.component.scss',
 })
 export class InternFormComponent {
-  // internForm nous permettra de manipuler les champs du formulaire
   public internForm: FormGroup = new FormGroup({});
 
-  // injection du formBuilder, du InternService et du Router dans le composant
   constructor(
     private _formBuilder: FormBuilder,
     private _internService: InternService,
     private _router: Router
   ) {}
 
-  // ngOnInit permet d'appliquer une méthode à tous les composants qui est exécutée immédiatement après le constructor de la classe
   ngOnInit(): void {
-    // normalement comme c'est un controller, ce qu'on crée ici devrait être dans un service car on construit un formulaire
-    // On aura de la duplication de code lorsqu'on fera le form pour modifier les données d'où l'intérêt de mettre ça dans un service
-    // group() pour grouper, on fait passer en arguments un objet
-    // dans cet objet : on fait passer les contrôles qu'on nomme souvent par le nom de l'input
     this.internForm = this._formBuilder.group({
       lastname: [
         '', //default value
