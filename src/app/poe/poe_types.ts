702866b62d2f2bfabfd61437af2185ee65ecef87
anneclaire <s36093@stagiaire.aelion.fr>
2024-03-29 11:07:43 +0100

    POE_US1-create class PoeType

---
+++ src/app/poe/poe_types.ts
@@ -0,0 +1,11 @@
+export class PoeType {
+       /**
+     * @var {string} label of the POE
+     */
+  public label: string = '';
+
+     /**
+     * @var {number} duration of the POE
+     */
+  public duration: number = 1;
+}
