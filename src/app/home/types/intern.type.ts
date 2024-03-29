4b476a8801aedb9d0a0173dfc6d8127f48e585e8
anneclaire <s36093@stagiaire.aelion.fr>
2024-03-29 16:04:44 +0100

    POE_US5-modify type Intern adding poe

--- src/app/home/types/intern.type.ts
+++ src/app/home/types/intern.type.ts
@@ -1,8 +1,7 @@
-/**Fichier dans lequel on déclare le type de données de l'attribut interns
- *Dans un type on ne peut définir que des propriétés
- */
+import { Poe } from '../../poe/poe_types/poe_types';
+
 export type Intern = {
   lastname: string;
   firstname: string;
+  poe: Poe;
 };
-
