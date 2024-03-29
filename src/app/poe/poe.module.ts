3ed52ee55b826fedf9f9fc668bcf031bef69c69a
anneclaire <s36093@stagiaire.aelion.fr>
2024-03-29 12:12:44 +0100

    POE_US2-Get the list in alphabetic order

--- src/app/poe/poe.module.ts
+++ src/app/poe/poe.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PoeComponent } from './poe/poe.component';
 import { AddPoeComponent } from './add-poe/add-poe.component';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    PoeComponent,
-    AddPoeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PoeComponent, AddPoeComponent],
+  imports: [CommonModule, HttpClientModule],
 })
-export class PoeModule { }
+export class PoeModule {}
--- src/app/poe/services/poe.service.ts
+++ src/app/poe/services/poe.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { IService } from '../../core/interfaces/i-service';
 import { Poe } from '../poe_types/poe_types';
 import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PoeService implements IService<Poe> {
-  private _poe: Array<Poe> = [
+  private _poes: Array<Poe> = [
     {
       label: 'Développeur en cybersécurité',
       duration: 457,
@@ -26,15 +27,24 @@ export class PoeService implements IService<Poe> {
     },
   ];
 
-  constructor() {}
-  add(item: Poe): void {
-    throw new Error('Method not implemented.');
+  constructor(private _httpClient: HttpClient) {}
+  add(item: Poe): Observable<Poe> {
+    return this._httpClient.post<Poe>('http://localhost:3000/poes', item);
   }
+
   findAll(): Observable<Poe[]> {
-    throw new Error('Method not implemented.');
+    return this._httpClient.get<Poe[]>('http://localhost:3000/poes').pipe(
+      map((poes: Poe[]) => {
+        return poes.sort((p1: Poe, p2: Poe) => {
+          return p1.label.localeCompare(p2.label);
+        });
+      })
+    );
   }
 
   get poe(): Array<Poe> {
-    return this._poe;
+    return this._poes.sort((p1: Poe, p2: Poe) => {
+      return p1.label.localeCompare(p2.label);
+    });
   }
 }
