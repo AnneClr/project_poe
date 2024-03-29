088559763953d627c60ba154392ba280e4f4ea13
anneclaire <s36093@stagiaire.aelion.fr>
2024-03-29 13:43:51 +0100

    POE_US5-added routermodule and reactiveformmodule

--- src/app/poe/poe.module.ts
+++ src/app/poe/poe.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PoeComponent } from './poe/poe.component';
 import { AddPoeComponent } from './add-poe/add-poe.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PoeComponent, AddPoeComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterModule, ReactiveFormsModule],
 })
 export class PoeModule {}
